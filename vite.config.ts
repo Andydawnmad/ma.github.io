6cdacf21a1c94f49b44a77edf70442dfca7d420d
Andydawnmad <ducnguyenar@gmail.com>
2024-10-31 13:31:53 +0700

    Cập nhật ứng dụng lên GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,25 +4,27 @@ import version from "vite-plugin-package-version";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ma.github.io/',
+  base: '/ma.github.io/', // Đường dẫn cơ sở cho GitHub Pages
   plugins: [react(), version()],
   build: {
     rollupOptions: {
       output: {
-        // manualChunks(id) {
-        //   if (id.includes("node_modules")) {
-        //     return id
-        //       .toString()
-        //       .split("node_modules/")[1]
-        //       .split("/")[0]
-        //       .toString();
-        //   }
-        // },
-        manualChunks: {
-          // Specify manual chunk names and modules
-          react: ["react", "react-dom"],
-          lottie: ["react-lottie"],
+        // Tùy chọn manualChunks đã được điều chỉnh
+        manualChunks(id) {
+          // Nếu id chứa 'node_modules', tách và trả về tên gói
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
         },
+        // Nếu bạn muốn tạo các chunk riêng cho một số thư viện, bạn có thể bỏ comment và sử dụng
+        // manualChunks: {
+        //   react: ["react", "react-dom"],
+        //   lottie: ["react-lottie"],
+        // },
       },
     },
   },
